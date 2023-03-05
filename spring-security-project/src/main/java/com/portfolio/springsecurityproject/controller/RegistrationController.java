45ee010930c6982c5c392fb1f029d83b69bf8dc9
timibolaji <mattybolaji@gmail.com>
2023-03-05 13:01:06 +0100

    [1]Added User Entity

---
+++ spring-security-project/src/main/java/com/portfolio/springsecurityproject/controller/RegistrationController.java
@@ -0,0 +1,2 @@
+package com.portfolio.springsecurityproject.controller;public class RegistrationController {
+}
---
+++ spring-security-project/src/main/java/com/portfolio/springsecurityproject/entity/User.java
@@ -0,0 +1,2 @@
+package com.portfolio.springsecurityproject.entity;public class User {
+}
